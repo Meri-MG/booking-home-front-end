9611f1b22f3959711c4518f3bf65143feb8eb5f5
youmari <youmari300@gmail.com>
2022-04-29 15:14:36 +0000

    Add register actions and reducer

---
+++ src/Redux/Register/Register.js
@@ -0,0 +1,38 @@
+import fetchData from '../../api/api';
+
+const initialState = {
+  IsLogged_in: false,
+  user: {},
+  errors: null,
+};
+
+export const SIGNUP_SUCCESS = 'BOOKING_HOME/SIGNUP_SUCCESS';
+export const SIGNUP_FAILURE = 'BOOKING_HOME/SIGNUP_FAILURE';
+
+export const signUserUp = (user) => async (dispatch) => {
+  try {
+    await fetchData
+      .post('sessions', user, { withCredentials: true })
+      .then((response) => dispatch({ type: SIGNUP_SUCCESS, data: response.data }));
+  } catch (error) {
+    dispatch({ type: SIGNUP_FAILURE, error });
+  }
+};
+
+const registerReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SIGNUP_SUCCESS:
+      return {
+        ...state,
+        user: action.data.user,
+        IsLogged_in: action.data.logged_in,
+      };
+    case SIGNUP_FAILURE:
+      return { ...state, errors: action.error };
+
+    default:
+      return state;
+  }
+};
+
+export default registerReducer;
