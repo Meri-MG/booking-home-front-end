a268ee208784ecac8f645a7e722393e9a051ab9e
youmari <youmari300@gmail.com>
2022-04-29 15:17:08 +0000

    Change store location

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import store from './redux/store';
+import store from './Redux/store';
 import './index.css';
 import App from './App';
 
