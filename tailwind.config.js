e0a091086f5d26d8ffc9d239e7716b3d2f36f6bf
youmari <youmari300@gmail.com>
2022-04-30 01:29:44 +0000

    Add main color and background image to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
-    colors: {
-      'main-color': '#96bf01',
+    extend: {
+      colors: {
+        'main-color': '#96bf01',
+        't-main-color': '#96bf01a4',
+      },
+      backgroundImage: {
+        'bg-image': "url('./Assets/apprt.avif')",
+      },
     },
   },
   plugins: [],
