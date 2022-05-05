e173c52de0eaf26c4e5e12bfbaddfabd447eacfe
zemola <azeemolatunji@gmail.com>
2022-05-05 09:06:55 +0100

    added on submit method

--- src/Components/createApartment.js
+++ src/Components/createApartment.js
@@ -22,13 +22,13 @@ const CreateApartment = () => {
     setNewApartment({ ...newApartment, [name]: value });
   };
 
-  // const onSubmit = async (e) => {
-  //   const token = getToken();
-  //   e.preventDefault();
-  //   await dispatch(CreateApartmentHandler(newApartment, token));
-  //   await dispatch(getApartments(token));
-  //   navigate('/Apartments');
-  // };
+  const onSubmit = async (e) => {
+    const token = getToken();
+    e.preventDefault();
+    await dispatch(CreateApartmentHandler(newApartment, token));
+    await dispatch(getApartments(token));
+    navigate('/Apartments');
+  };
 
 
   return (
@@ -38,14 +38,14 @@ const CreateApartment = () => {
       </header>
       <div>
         <form onSubmit={(e) => onSubmit(e)}>
-          <input type="text" className="form-control m-1" id="name" placeholder="Enter Name" name="name" onChange={(e) => handleChange(e)} style={divStyle} />
+          <input type="text"  id="name" placeholder="Enter Name" name="name" onChange={(e) => handleChange(e)} style={divStyle} />
           <textarea rows="4" cols="50" type="text" className="form-control m-1" id="description" placeholder="Enter Description" name="description" onChange={(e) => handleChange(e)} style={divStyle} />
-          <input type="text" className="form-control m-1" id="image" placeholder="Enter Image Url" name="image" onChange={(e) => handleChange(e)} style={divStyle} />
-          <input type="text" className="form-control m-1" id="city" placeholder="Enter City" name="city" onChange={(e) => handleChange(e)} style={divStyle} />
-          <input type="number" className="form-control m-1" id="capacity" placeholder="Enter capacity" name="capacity" onChange={(e) => handleChange(e)} style={divStyle} />
-          <input type="number" className="form-control m-1" id="price" placeholder="Enter Price" name="price" onChange={(e) => handleChange(e)} style={divStyle} />
-          <div className="d-flex justify-content-end">
-            <button type="submit" className="btn btn-primary m-1">Submit</button>
+          <input type="text" id="image" placeholder="Enter Image Url" name="image" onChange={(e) => handleChange(e)} style={divStyle} />
+          <input type="text" id="city" placeholder="Enter City" name="city" onChange={(e) => handleChange(e)} style={divStyle} />
+          <input type="number" id="capacity" placeholder="Enter capacity" name="capacity" onChange={(e) => handleChange(e)} style={divStyle} />
+          <input type="number" id="price" placeholder="Enter Price" name="price" onChange={(e) => handleChange(e)} style={divStyle} />
+          <div>
+            <button type="submit" >Submit</button>
           </div>
         </form>
 
