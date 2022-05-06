82c75823c7ad177b5e70959e38e25216298c8d88
Meri <mgogi13@freeuni.edu.ge>
2022-05-06 18:28:53 +0400

    feat: install necessary files

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { Route, Routes } from 'react-router';
 import Home from './Components/Home';
 import LoginForm from './Components/LoginForm';
 import SignupFrom from './Components/SignupFrom';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route element={<Home />} path="/">
-          <Route element={<LoginForm />} path="/Login" />
-          <Route element={<SignupFrom />} path="/Register" />
-        </Route>
-      </Routes>
+      <>
+        <Sidebar />
+        <Routes>
+          <Route element={<Home />} path="/">
+            <Route element={<LoginForm />} path="/Login" />
+            <Route element={<SignupFrom />} path="/Register" />
+          </Route>
+        </Routes>
+      </>
     </div>
   );
 }
