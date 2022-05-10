51dde75ee0d49099b3028eef8b854137c2156e2d
mong'are <mongaresams@gmail.com>
2022-05-10 07:57:01 +0300

    Add styled components, react-icons and Hero section to app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import Home from './Components/Home';
 import LoginForm from './Components/LoginForm';
 import SignupFrom from './Components/SignupFrom';
 import Sidebar from './Components/Sidebar';
+import Hero from './Components/Hero';
+import SliderData from './data/SliderData';
+import GlobalStyle from './globalStyles';
 
 function App() {
   return (
     <div className="App">
       <>
+        <GlobalStyle />
         <Sidebar />
         <Routes>
+          <Route element={<Hero slides={SliderData} />} path="/apartments" />
           <Route element={<Home />} path="/">
             <Route element={<LoginForm />} path="/Login" />
             <Route element={<SignupFrom />} path="/Register" />
