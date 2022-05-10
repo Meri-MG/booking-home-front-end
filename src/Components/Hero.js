c1603a71a2814bc694d8c55a63be74f2b1382cb7
mong'are <mongaresams@gmail.com>
2022-05-10 08:02:14 +0300

    Add styled components basic styling to hero component

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,6 +5,88 @@ import { IoIosArrowRoundForward } from 'react-icons/io';
 import { IoArrowForward, IoArrowBack } from 'react-icons/io5';
 import Button from './Button';
 
+const HeroSection = styled.section`
+  height: 100vh;
+  max-height: 1100px;
+  position: relative;
+  overflow: hidden;
+`;
+
+const HeroWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
+`;
+
+const HeroSlide = styled.div`
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+`;
+
+const HeroSlider = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  $::before {
+    content: '';
+    position: absolute;
+    z-index: 2;
+    width: 100%;
+    height: 100vh;
+    bottom: 0vh;
+    left: 0;
+    overflow: hidden;
+    opacity: 0.4;
+    background: linear-gradient(odeg,
+       rgba(0, 0, 0, 0.2) 0%,
+       rgba(0, 0, 0, 0.2) 50%,
+       rgba(0, 0, 0, 0.6) 100%);
+`;
+
+const HeroImage = styled.img`
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+  object-position: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const HeroContent = styled.div`
+    position: relative;
+    z-index: 3;
+    display: flex;
+    flex-direction: column;
+    max-width: 1600px;
+    width: calc(100% - 100px);
+    color: white;
+
+    h1 {
+      font-size: clamp(1rem, 8vw, 2rem);
+      font-weight: 400;
+      text-transform: uppercase;
+      text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
+      text-align: left;
+      margin-bottom: 0.8rem;
+    } 
+
+    p {
+      margin-bottom: 1.2rem;
+      text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
+    }
+`;
 
 
 const Hero = ({ slides }) => {
