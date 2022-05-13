8384e7b2f2defdd2e58270e7e5611cdf436fd5a2
mohamedelshamy55 <mohamedelshamy55@gmail.com>
2022-05-13 12:47:27 +0200

    update store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -5,12 +5,14 @@ import loginReducer from './Login/Login';
 import registerReducer from './Register/Register';
 import apartmentsReducer from './Apartments/Apartments';
 import apartmentsDetailsReducer from './Details/Details';
+import reservationsReducer from './Reserved/Reserved';
 
 const reducers = combineReducers({
   LogIn: loginReducer,
   Register: registerReducer,
   Apartments: apartmentsReducer,
   Details: apartmentsDetailsReducer,
+  Reservations: reservationsReducer,
 });
 
 const middlewares = [thunk, logger];
